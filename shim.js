480fc19caf084523e89518da2d3af2e0a3f987ee
Shruti3004 <mail2shruti.ag@gmail.com>
2022-11-23 11:11:04 +0530

    fix: local setup
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- shim.js
+++ shim.js
@@ -25,4 +25,22 @@ if (typeof localStorage !== 'undefined') {
 // crypto is loaded first, so it can populate global.crypto
 // require('crypto')
 
-if (typeof BigInt === 'undefined') global.BigInt = require('big-integer');
+const bi = require('big-integer');
+
+function patchedBigInt(value) {
+  if (typeof value === 'string') {
+    const match = value.match(/^0([xo])([0-9a-f]+)$/i);
+    if (match) {
+      return bi(match[2], match[1].toLowerCase() === 'x' ? 16 : 8);
+    }
+  }
+  return bi(value);
+}
+
+if (typeof BigInt === 'undefined') {
+  if (Platform.OS === 'android') {
+    global.BigInt = patchedBigInt;
+  } else {
+    global.BigInt = bi;
+  }
+}
